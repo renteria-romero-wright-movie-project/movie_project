2ab263fdd1635a50c916818ffc07e6f9deac1bba
David Renteria <david.j.renteria1@gmail.com>
2023-03-21 11:49:08 -0500

    still working pt 2

--- movie_project.js
+++ movie_project.js
@@ -1,1 +1,21 @@
-$.get('https://longing-flossy-bed?path=README.md%3A1%3A0.glitch.me/movies');
+// $.get('https://longing-flossy-bed.glitch.me/');
+// const reviewObj = {
+//     restaurant_id: 1,
+//     name: 'Codey',
+//     rating: 5,
+//     comments: "This is a really good place for coding and eating"
+// };
+// const url = 'https://longing-flossy-bed.glitch.me/';
+// const options = {
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(reviewObj),
+// };
+fetch('https://longing-flossy-bed.glitch.me/movies')
+    .then( response => response.json() ).then(movies => {
+        movies.forEach(movie => {
+            console.log(movie);
+        })})
+
+ /* review was created successfully */
