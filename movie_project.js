a0f245270f647b254173770ce1725941ba80cb89
Ciera Romero-Wright <romerowrightciera@gmail.com>
2023-03-21 11:51:45 -0500

    some variables created

--- movie_project.js
+++ movie_project.js
@@ -1,1 +1,7 @@
 $.get('https://longing-flossy-bed?path=README.md%3A1%3A0.glitch.me/movies');
+onDOMContentLoaded
+let movieRating = document.getElementById('movie')
+
+var loadingSign = document.getElementsByTagName('body')
+alert('Please be patient while the page loads...');
+ console.log(loadingSign)
