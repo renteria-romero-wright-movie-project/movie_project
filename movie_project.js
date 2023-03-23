7c9af20e3afea9579ba865230eef6ee9d17ea914
David Renteria <david.j.renteria1@gmail.com>
2023-03-22 21:25:47 -0500

    deletes showing in db.json

--- movie_project.js
+++ movie_project.js
@@ -1,10 +1,11 @@
 (() => {
     "use strict";
-
     let loadMessage = $('#page-loading').toggleClass('hidden');
     let submitBtn = document.querySelector('#movie-choice');
     let submitNewMovie = document.querySelector('#newMovieBtn');
     let filterMovie = document.querySelector('#movie-cards');
+
+
     pageLoaded();
     console.log(submitNewMovie);
 
@@ -36,145 +37,148 @@
 
     //DOM manip
 
-    function pageLoaded() {
-        $(document).ready(function(){
+        function pageLoaded() {
+            $(document).ready(function () {
             $('#page-loading').show();
-        });
-        let movies = document.getElementById('movie-cards');
-        fetch('https://longing-flossy-bed.glitch.me/movies')
-            .then(response => response.json())
-            .then(movie => {
-                let html = '';
+    });
 
-                for (let i = 0; i < movie.length; i++) {
-                    html += `<div>`
-                    html += `<h2>${movie[i].title}</h2>`
-                    html += `<h6>Rating: ${movie[i].rate} stars</h6>`
-                    html += `<h6>Genre: ${movie[i].genre}</h6>`
-                    html += `<button name="Save" id="save-Movies" type="submit" value="${movie[i].id}">Save</button>`
-                    html += `<button name="Edit" id="edit-Movies" type="submit" value="${movie[i].id}">Edit</button>`
-                    html += `<button name="Delete" id="delete-Movies" type="submit" value="${movie[i].id}">Delete</button>`
-                    html += `</div>`
-                }
-                movies.innerHTML = html;
-                // edit btn - may need extra toggle class line-43
-                $('#edit-Movies').click(function () {
-                    let editPic = ($(this).val());
-                    let titlePic1 = $(this).parent().children('h2').first().html()
-                    let ratingPic1 = $(this).parent().children('h6').first().html()
-                    $(this).parent().children('#save-Movies').toggleClass('hidden')
-
-                    $(this).parent().children('h2').first().html(`<input type='text' value='${title}' id="makeChange">`);
-                    $(this).parent().children('h6').first().html(changeRating(rating));
-
-                    //save btn
-
-                    $('#save-Movies').click(function () {
-                        let titlePic2 = $('#makeChange').val();
-                        let ratingPic2 = $("#selector").val();
-                        changeMovies(editPic, titlePic2, ratingPic2);
-                    });
-
-                    // change title
-                    $('#makeChange').click(function (e) {
-                        let typing = e.key;
-                        let inputBox = $(this).val();
-                        let ratingPic3 = $('#selector').val();
-                        changeMovies(editPic, inputBox, ratingPic3);
-                    });
-
-                    // change rating
-
-                    $('.changeRate').click(function (e) {
-                        let typing = e.key;
-                        let inputBox = $('#makeChange').val();
-                        let ratingPic4 = $('#selector').val();
-                        changeMovies(editPic, inputBox, ratingPic4);
-                    });
-                })
-            })
-        $('#delete-Movies').on(click,deleteMovies())
-
-
-        //deleting movies
-
-        function deleteMovies() {
-            fetch('https://longing-flossy-bed.glitch.me/movies/260', {
-                method: 'DELETE'
-            }).then(() => fetch('https://longing-flossy-bed.glitch.me/movies'))
-                .then(res => res.json())
-                .then(() => pageLoaded());
-        }
-            deleteMovies();
-
-        //editing movies
-
-        function changeMovies(chngFilm, title, rating) {
-            let changedFilm = {
-                name: title,
-                rate: rating
-            };
-            fetch('https://longing-flossy-bed.glitch.me/movies' + chngFilm, {
-                method: 'PUT',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(changedFilm)
-            })
-                .then(() => fetch('https://longing-flossy-bed.glitch.me/movies'))
-                .then(res => res.json())
-                .then(() => pageLoaded());
-        }
-
-        //change ratings
-        function changeRating(rate) {
+    let movies = document.getElementById('movie-cards');
+    fetch('https://longing-flossy-bed.glitch.me/movies')
+        .then(response => response.json())
+        .then(movie => {
             let html = '';
-            if (rate === 1) {
-                html += `<select id="selector" class="changeRate">
+
+            for (let i = 0; i < movie.length; i++) {
+                html += `<div>`
+                html += `<h2>${movie[i].title}</h2>`
+                html += `<h6>Rating: ${movie[i].rate} stars</h6>`
+                html += `<h6>Genre: ${movie[i].genre}</h6>`
+                html += `<button name="Save" id="save-Movies" type="submit" value="${movie[i].id}">Save</button>`
+                html += `<button name="Edit" id="edit-Movies" type="submit" value="${movie[i].id}">Edit</button>`
+                html += `<button name="Delete" id="delete-Movies" type="submit" value="${movie[i].id}">Delete</button>`
+                html += `</div>`
+            }
+            movies.innerHTML = html;
+
+
+            $('#edit-Movies').click(function () {
+                let editPic = ($(this).val());
+                let titlePic1 = $(this).parent().children('h2').first().html()
+                let ratingPic1 = $(this).parent().children('h6').first().html()
+                $(this).parent().children('#save-Movies').toggleClass('hidden')
+
+                $(this).parent().children('h2').first().html(`<input type='text' value='${title}' id="makeChange">`);
+                $(this).parent().children('h6').first().html(changeRating(rating));
+
+                //save btn
+
+                $('#save-Movies').click(function () {
+                    let titlePic2 = $('#makeChange').val();
+                    let ratingPic2 = $("#selector").val();
+                    changeMovies(editPic, titlePic2, ratingPic2);
+                });
+
+                // change title
+                $('#makeChange').click(function (e) {
+                    let typing = e.key;
+                    let inputBox = $(this).val();
+                    let ratingPic3 = $('#selector').val();
+                    changeMovies(editPic, inputBox, ratingPic3);
+                });
+
+                // change rating
+
+                $('.changeRate').click(function (e) {
+                    let typing = e.key;
+                    let inputBox = $('#makeChange').val();
+                    let ratingPic4 = $('#selector').val();
+                    changeMovies(editPic, inputBox, ratingPic4);
+                });
+            })
+        })
+    $('#delete-Movies').on(click, deleteMovies())
+
+
+    //deleting movies
+
+    function deleteMovies() {
+        fetch('https://longing-flossy-bed.glitch.me/movies/260', {
+            method: 'DELETE'
+        }).then(() => fetch('https://longing-flossy-bed.glitch.me/movies'))
+            .then(res => res.json())
+            .then(() => pageLoaded());
+    }
+
+    deleteMovies();
+
+    //editing movies
+
+    function changeMovies(chngFilm, title, rating) {
+        let changedFilm = {
+            name: title,
+            rate: rating
+        };
+        fetch('https://longing-flossy-bed.glitch.me/movies' + chngFilm, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(changedFilm)
+        })
+            .then(() => fetch('https://longing-flossy-bed.glitch.me/movies'))
+            .then(res => res.json())
+            .then(() => pageLoaded());
+    }
+
+    //change ratings
+    function changeRating(rate) {
+        let html = '';
+        if (rate === 1) {
+            html += `<select id="selector" class="changeRate">
                     <option selected value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
                     <option value="5">5</option>
                 </select>`
-                return html;
-            } else if (rating == 2) {
-                html += `<select id="selector" class="changeRate">
+            return html;
+        } else if (rating == 2) {
+            html += `<select id="selector" class="changeRate">
                     <option value="1">1</option>
                     <option selected value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
                     <option value="5">5</option>
                 </select>`
-                return html;
-            } else if (rating == 3) {
-                html += `<select id="selector" class="changeRate">
+            return html;
+        } else if (rating == 3) {
+            html += `<select id="selector" class="changeRate">
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option selected value="3">3</option>
                     <option value="4">4</option>
                     <option value="5">5</option>
                 </select>`
-                return html;
-            } else if (rating == 4) {
-                html += `<select id="selector" class="changeRate">
+            return html;
+        } else if (rating == 4) {
+            html += `<select id="selector" class="changeRate">
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option selected value="4">4</option>
                     <option value="5">5</option>
                 </select>`
-                return html;
-            } else if (rating == 5) {
-                html += `<select id="selector" class="changeRate">
+            return html;
+        } else if (rating == 5) {
+            html += `<select id="selector" class="changeRate">
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
                     <option selected value="5">5</option>
                 </select>`
-                return html;
-            }
+            return html;
         }
     }
+}
 })();
 
 
